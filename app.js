db7c0a00003673aacd96f569fb322380dae388a9
otiscom <matlipi65@gmail.com>
2025-11-29 21:36:20 +0100

    LAB2 EJS Fix

--- app.js
+++ app.js
@@ -4,22 +4,19 @@ const express = require('express');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 const app = express();
 
-app.engine('ejs', engine({extname: 'ejs', defaultLayout: 'main-layout'}));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path:'/' });
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/' });
 });
 
 app.listen(33333);
-
--- routes/admin.js
+++ routes/admin.js
@@ -20,7 +20,7 @@ router.get('/add-product', (req, res, next) => {
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
   const price = Math.floor(Math.random() * 90)+10;
-  const desc = "awesome product" + req.body.title;
+  const desc = "Awesome product " + req.body.title;
   products.push({ title: req.body.title, price: price, desc: desc });
   //console.log(req.body);
   res.redirect('/');
